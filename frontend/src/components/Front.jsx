1fa5830c93ea7335a47ccd8eac1e80fe7cac8a66
jjplaza-dev <jjplaza.dev@gmail.com>
2025-05-28 23:14:26 +0800

    Front animations

--- frontend/src/components/Front.jsx
+++ frontend/src/components/Front.jsx
@@ -52,7 +52,35 @@ useEffect(() => {
   const myName = "Jurist Plaza"
   const myNameArr = myName.split("")
 
+  const wordText = document.querySelectorAll("#wordText")
+  const titleText = document.querySelectorAll("#titleText")
 
+  let indexWord = 0;
+  let indexTitle = 0
+
+function addEffect() {
+  if (indexWord >= wordText.length) return;
+  
+  wordText[indexWord].style.transform = "rotate(0deg)";
+  indexWord++;
+
+  setTimeout(addEffect, 50); // Add effect to next span every second
+}
+function addEffect2() {
+  if (indexTitle >= titleText.length) return;
+  
+  titleText[indexTitle].style.transform = "translateY(0%)";
+  indexTitle++;
+  
+  setTimeout(addEffect2, 50); // Add effect to next span every second
+}
+
+addEffect();
+addEffect2();
+  
+const primary = "#303030";
+const secondary = "white"
+const accent = "#a4a4a4"
   return <>
     <section className='w-full h-[200vh] relative bg-white'>
       
@@ -65,16 +93,36 @@ useEffect(() => {
                 <div className={`w-full h-full flex flex-col lg:flex-row text-[10vw] sm:text-[8vw] lg:text-[7vw]  `}>
                   <div className='w-full h-full text-nowrap flex items-center'>
                     {myWordArr.map((text, index) => (
-                      <span className='text-[15vw] lg:text-[10vw] m-[2px] inline-block tracking-tighter leading-[1] poiret-one-regular rotate-y-0 hover:rotate-y-90 duration-700' key={index} id='wordText' >{text}</span>
+                      <span className='text-[15vw] lg:text-[10vw] m-[2px] inline-block tracking-tighter leading-[1] poiret-one-regular rotate-y-90 hover:rotate-90 duration-700' key={index} id='wordText' >{text}</span>
                     ))}
                   </div>
-                  <div className='w-full h-full '></div>  
+                  <div className='w-full h-full overflow-hidden relative bg-black'>
+                    <Hablon width={30} height={2000} rotate={90} positionX={10} color={"white"}/>  
+    
+                    <Hablon width={50} height={400} rotate={0} positionX={0} positionY={0} color={secondary}/>
+                    <Hablon width={50} height={400} rotate={0} positionX={50} positionY={0} color={accent}/>
+                    <Hablon width={50} height={400} rotate={0} positionX={100} positionY={0} color={secondary}/>
+                    <Hablon width={50} height={400} rotate={0} positionX={150} positionY={0} color={primary}/>
+                    <Hablon width={50} height={400} rotate={0} positionX={200} positionY={0} color={primary}/>
+                    <Hablon width={50} height={400} rotate={0} positionX={250} positionY={0} color={secondary}/>
+                    <Hablon width={50} height={400} rotate={0} positionX={300} positionY={0} color={accent}/>
+                    <Hablon width={50} height={400} rotate={0} positionX={350} positionY={0} color={secondary}/>
+                    <Hablon width={50} height={400} rotate={0} positionX={400} positionY={0} color={primary}/>
+                    <Hablon width={50} height={400} rotate={0} positionX={450} positionY={0} color={primary}/>
+                    <Hablon width={50} height={400} rotate={0} positionX={500} positionY={0} color={secondary}/>
+                    <Hablon width={50} height={400} rotate={0} positionX={550} positionY={0} color={accent}/>
+                    <Hablon width={50} height={400} rotate={0} positionX={600} positionY={0} color={secondary}/>
+                    <Hablon width={50} height={400} rotate={0} positionX={650} positionY={0} color={primary}/>
+                   
+                    
+                    
+                  </div>  
                 </div>
                 
                 <div className='w-full h-full flex flex-col lg:flex-row-reverse text-[10vw] sm:text-[8vw] lg:text-[7vw] items-center text-nowrap'>
                    <div className='w-full h-full text-nowrap flex items-center'>
                     {myTitleArr.map((text, index) => (
-                      <span className='text-[12vw] lg:text-[10vw] m-[1px] min-h-[10px] min-w-[8px] lg:min-w-[20px] inline-block tracking-tighter leading-[1] poiret-one-regular' key={index} id='titleText'>{text}</span>
+                      <span className='text-[12vw] lg:text-[10vw] m-[1px] min-h-[10px] min-w-[8px] lg:min-w-[20px] inline-block tracking-tighter leading-[1] overflow-hidden' key={index}><div className="w-fit h-fit poiret-one-regular duration-500" style={{ transform: "translateY(150%) scaleY(250%)"}} id='titleText'>{text}</div></span>
                     ))}
                   </div>
                   <div className='w-full h-full relative'> 
@@ -102,6 +150,18 @@ objectives and boosts conversions.</div>
       </div>
     </section>
   </>
+}
+
+const Hablon = (props) => {
+  const strandsNum = Math.ceil(props.width/2)
+  console.log(strandsNum)
+  return <section className='absolute mx-0 top-[50%] translate-y-[-50%]' style={{transform: ` translateX(${props.positionX}px) translateY(${props.positionY}px)`, transformOrigin: `center`}}>
+    <div className='flex' style={{width: `${props.width}px`, height: `${props.height}px`, transform: `rotate(${props.rotate}deg)`}}>
+      {Array.from({ length: strandsNum }, (x, i) => (
+       <div key={i} className='w-[1px] h-full mr-[1px]' style={{backgroundColor: `${props.color}`}}></div>
+      ))}
+    </div>
+  </section>
 }
 
 export default Front
