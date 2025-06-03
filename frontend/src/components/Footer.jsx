db3e0512a8f6190f80b30aef4c3afbe109efc94a
jjplaza-dev <jjplaza.dev@gmail.com>
2025-06-03 23:19:07 +0800

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -28,7 +28,7 @@ function Footer() {
 
         <div className='w-full h-[80vh] absolute top-[10vh] bg-blackoverflow-hidden text-white'>
             <div className='w-full h-full absolute top-0 z-1'>
-              <div className='w-full h-fit text-[80px] md:text-[150px] lg:text-[200px] absolute top-[50%] translate-y-[-50%] opacity-20 poiret-one-regular font-extrabold text-center tracking-[0.06em] text-nowrap'>LET'S TALK</div>
+              <button className='w-fit h-fit text-[60px] md:text-[150px] lg:text-[200px] absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] opacity-20 poiret-one-regular font-extrabold text-center tracking-[0.06em] text-nowrap px-2 lg:px-10'>LET'S TALK</button>
              </div>
         </div>
 
